74a43cef76dca099abd4bda2dbd8ed37a1ccfa40
David Andrew Towers <d.towers2@ncl.ac.uk>
2019-11-06 15:53:04 +0000

    Base 7 - Missing texture on Glass

--- 7) Scene Management/Renderer.h
+++ 7) Scene Management/Renderer.h
@@ -3,7 +3,9 @@
 #include "../../nclgl/OGLRenderer.h"
 #include "../../nclgl/Camera.h"
 #include "../../nclgl/SceneNode.h"
+#include "../../nclgl/Frustrum.h"
 #include "../../nclgl/CubeRobot.h"
+#include <algorithm>
 
 class Renderer : public OGLRenderer {
 public:
@@ -14,8 +16,18 @@ public:
 	virtual void RenderScene();
 
 protected:
+	void BuildNodeLists(SceneNode* from);
+	void SortNodeLists();
+	void ClearNodeLists();
+	void DrawNodes();
 	void DrawNode(SceneNode* n);
 
 	SceneNode* root;
 	Camera* camera;
+	Mesh* quad;
+
+	Frustrum frameFrustum;
+
+	vector<SceneNode*> transparentNodeList;
+	vector<SceneNode*> nodeList;
 };
