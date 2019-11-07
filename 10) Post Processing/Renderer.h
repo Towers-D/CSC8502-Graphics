43cbed7b3d2f55173f39162a231356991373f7cc
David Andrew Towers <d.towers2@ncl.ac.uk>
2019-11-07 16:31:04 +0000

    Tutorial 10 base ask about red

---
+++ 10) Post Processing/Renderer.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "../../nclgl/OGLRenderer.h"
+#include "../../nclgl/HeightMap.h"
+#include "../../nclgl/Camera.h"
+
+#define POST_PASSES 10
+
+class Renderer : public OGLRenderer {
+public:
+	Renderer(Window& parent);
+	virtual ~Renderer();
+
+	virtual void RenderScene();
+	virtual void UpdateScene(float msec);
+
+protected:
+	void PresentScene();
+	void DrawPostProcess();
+	void DrawScene();
+
+	Shader* sceneShader;
+	Shader* processShader;
+
+	Camera* camera;
+
+	Mesh* quad;
+	HeightMap* heightMap;
+
+	GLuint bufferFBO;
+	GLuint processFBO;
+	GLuint bufferColourTex[2];
+	GLuint bufferDepthTex;
+};
